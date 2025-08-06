a3b166d20d145ab6f2923944f376444d5b3b74da
cccop18 <arthurhangel@gmail.com>
2025-08-05 19:12:20 -0500

    feat: atualiza projeto com novos arquivos trocados

--- sgcmapi/src/main/java/br/ufac/sgcmapi/model/Unidade.java
+++ sgcmapi/src/main/java/br/ufac/sgcmapi/model/Unidade.java
@@ -2,10 +2,23 @@ package br.ufac.sgcmapi.model;
 
 import java.io.Serializable;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
 public class Unidade implements Serializable {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(nullable = false, updatable = false)
     private Long id;
+
+    @Column(nullable = false, unique = true)
     private String nome;
+
     private String endereco;
     
     public Long getId() {
